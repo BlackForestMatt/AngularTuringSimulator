55c841ce1f84796bf26c345fb0c80a8880680d78
BlackForestMatt <matthias.muelhaupt@gmail.com>
2016-07-21 19:17:00 +0200

    Added comments

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,10 +1,13 @@
-interface token {
+export interface token {
   type: string;
   value: string;
   pos: number;
 }
 
-class Lexer {
+/**
+ * Lexical analyizer for Turing-lang
+ */
+export class Lexer {
   private terminalAlphabet: string[] = [
     'start',
     'blank',
@@ -31,31 +34,56 @@ class Lexer {
     this.pos = 0;
   }
 
+  /**
+   * Checks if input pointer is still in bounds
+   * @return in input bounds
+   */
   private inBounds(): boolean {
     return this.pos < this.len;
   }
 
+  /**
+   * Get the current character in the input that is pointed to by pos
+   * @return current char
+   */
   private getChar(): string {
     return this.source.charAt(this.pos);
   }
 
+  /**
+   * Checks if given character is a newline or carriage return char
+   * @param char The character to check
+   * @return Char is a newline char
+   */
   private isNewline(char: string): boolean {
     return char == '\r' || char == '\n';
   }
 
+  /**
+   * Checks if given character is a space or tab char
+   * @param char The character to check
+   * @return Char is a space char
+   */
   private isSpace(char: string): boolean {
     return char == ' ' || char == '\t';
   }
 
-  private isNumeric(char: string): boolean {
-    return (char >= '0' && char <= '9');
-  }
-
-  private isAplha(char: string): boolean {
+  /**
+   * Checks if given character is an alphanumeric value
+   * @param char The character to check
+   * @return Char is an alphanumeric value
+   */
+  private isAplhaNumeric(char: string): boolean {
     return (char >= 'a' && char <= 'z') ||
-           (char >= 'A' && char <= 'Z');
+           (char >= 'A' && char <= 'Z') ||
+           (char >= '0' && char <= '9');
   }
 
+  /**
+   * Get the terminalAlphabet index of the next input
+   * Returns -1 if the next input is not a terminal symbol
+   * @return terminalAlphabet index of input
+   */
   private alphabetIndex(): number {
     let alphIndex: number = -1;
     let iPos: number;
@@ -79,7 +107,10 @@ class Lexer {
     return alphIndex;
   }
 
-  private skipWhiteSpaces() {
+  /**
+   * Skips whitespaces and newline characters in the input
+   */
+  private skipWhiteSpaces(): void {
     let char: string = this.getChar();
     while (this.inBounds() && (this.isSpace(char) || this.isNewline(char))) {
       this.pos++;
@@ -87,7 +118,11 @@ class Lexer {
     }
   }
 
-  private parseComment() {
+  /**
+   * Skips all input that is marked as a comment by a leading '/'
+   * a comment ends at the next newline character
+   */
+  private parseComment(): void {
     let char: string = this.getChar();
     while (this.inBounds() && !this.isNewline(char)) {
       this.pos++;
@@ -95,11 +130,17 @@ class Lexer {
     }
   }
 
+  /**
+   * Parses the next input as an identifier
+   * and returns a token describing the identifier.
+   * An identifier is an alphanumeric sequence including '_'.
+   * @return a token containing the next identifier
+   */
   private parseIdentifier(): token {
     let tok: token;
     let start: number = this.pos;
     let char: string = this.getChar();
-    while (this.inBounds() && (this.isAplha(char) || this.isNumeric(char) || char == '_')) {
+    while (this.inBounds() && (this.isAplhaNumeric(char) || char == '_')) {
       this.pos++;
       char = this.getChar();
     }
@@ -111,6 +152,12 @@ class Lexer {
     return tok;
   }
 
+  /**
+   * The scanner method to tokenize the given input.
+   * Throws an Error on unexpected input.
+   * @param source The source code to tokenize
+   * @return an array of tokens
+   */
   tokenize(source: string): token[] {
     this.source = source;
     this.len = source.length;
@@ -131,7 +178,7 @@ class Lexer {
         }
         this.pos += terminal.length;
         this.tokens.push(tok);
-      } else if ((this.isAplha(char) || this.isNumeric(char) || char == '_')) {
+      } else if ((this.isAplhaNumeric(char) || char == '_')) {
         this.tokens.push(this.parseIdentifier());
       } else {
         throw new Error('Unexpected character: ' + char);
