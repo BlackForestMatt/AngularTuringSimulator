de05d532a731633f7874c7678648be77c8de188d
1Jo1 <josef.grieb@t-online.de>
2017-01-10 22:31:20 +0100

    remove canvas component and width directive

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CanvasComponent } from './canvas.component';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { TuringMachineComponent } from './turing-machine/turing-machine.component';
 import { TsAnimationComponent } from './turing-machine/ts-animation/ts-animation.component';
 
 import { EditorComponent } from './turing-machine/editor/editor.component';
 import { ChartComponent } from './turing-machine/chart/chart.component';
-import { ContaineWidthDirectiveDirective } from './containe-width-directive.directive';
 import { LoadExampleComponent } from './turing-machine/load-example/load-example.component';
 import { AboutComponent } from './turing-machine/about/about.component';
 import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
@@ -21,14 +19,12 @@ import {TuringmachineService} from "./service/turingmachineservice.service";
 @NgModule({
   declarations: [
     AppComponent,
-    CanvasComponent,
     TuringMachineComponent,
     TsAnimationComponent,
     EditorComponent,
     ChartComponent,
     EditorComponent,
     ChartComponent,
-    ContaineWidthDirectiveDirective,
     LoadExampleComponent,
     AboutComponent
   ],
