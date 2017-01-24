701c8a0af6ad70cbc8d2a1c03fc90b510fa0a4a2
Josef <griebj95@gmail.com>
2017-01-24 22:52:52 +0100

    include ExampleData

--- Website/src/app/turing-machine/load-example/load-example.component.ts
+++ Website/src/app/turing-machine/load-example/load-example.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {ExampleData} from "../ExampleData";
 
 @Component({
   selector: 'load-example',
@@ -6,44 +7,6 @@ import {Component, OnInit, EventEmitter, Output} from '@angular/core';
   styleUrls: ['./load-example.component.css']
 })
 export class LoadExampleComponent implements OnInit {
-
-  private deleteB = "blank := _; / symbol to use as blank \n " +
-    "start := s0; / set start newState \n" +
-    "end := {s5}; \n" +
-    "/ transition function \n" +
-    "s0, a -> s1, a, >; \n" +
-    "s0, b -> s0, b, >; \n" +
-    "s0, _ -> s5, _, <; \n" +
-    "s1, a -> s1, a, >; \n" +
-    "s1, b -> s2, a, <; \n" +
-    "s1, _ -> s4, _, <; \n" +
-    "s2, a -> s2, a, <; \n" +
-    "s2, b -> s3, b, >; \n" +
-    "s2, _ -> s3, _, >; \n" +
-    "s3, a -> s0, b, >; \n" +
-    "s4, a -> s4, _, <; \n" +
-    "s4, b -> s4, b, <; \n"+
-    "s4, _ -> s5, _, >; \n";
-
-  private palindrom = "blank := _; \n" +
-    "start := s0; / set start newState \n" +
-    "end := {s6}; \n" +
-    "s0, a -> s1, _, >; \n" +
-    "s1, a -> s1, a, >; \n" +
-    "s2, a -> s2, a, >; \n" +
-    "s3, a -> s5, _, <; \n" +
-    "s5, a -> s5, a, <; \n" +
-    "s0, b -> s2, _, >; \n" +
-    "s1, b -> s1, b, >; \n" +
-    "s2, b -> s2, b, >; \n" +
-    "s4, b -> s5, _, <; \n" +
-    "s5, b -> s5, b, <; \n" +
-    "s0, _ -> s6, _, -; \n" +
-    "s1, _ -> s3, _, <; \n" +
-    "s2, _ -> s4, _, <; \n" +
-    "s3, _ -> s6, _, -; \n" +
-    "s4, _ -> s6, _, -; \n" +
-    "s5, _ -> s0, _, >; \n";
 
   @Output()
   onLoaded = new EventEmitter<Array<string>>(); //need for pass the data to EditorComponent
@@ -61,11 +24,11 @@ export class LoadExampleComponent implements OnInit {
 
     switch(example) {
       case "Delete a":
-        let loadExample = [example,this.deleteB];
+        let loadExample = [example,ExampleData.deleteA];
         this.onLoaded.emit(loadExample);
         break;
       case "Palindrome":
-        let palindrom = [example,this.palindrom];
+        let palindrom = [example,ExampleData.palindrom];
         this.onLoaded.emit(palindrom);
       default:
         break;
